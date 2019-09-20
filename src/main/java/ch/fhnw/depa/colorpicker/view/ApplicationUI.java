8bfedcf777e791f65842cf1f0698fb831cbbf98b
Florian Thiévent <florianjeremias@gmail.com>
2019-09-21 00:25:27 +0200

    [fth] added UI Items

--- src/main/java/ch/fhnw/depa/colorpicker/view/ApplicationUI.java
+++ src/main/java/ch/fhnw/depa/colorpicker/view/ApplicationUI.java
@@ -14,6 +14,17 @@ public class ApplicationUI extends BorderPane {
 
     private MenuItem closeItem;
 
+    private ToggleGroup colorGroup;
+    private RadioMenuItem redItem;
+    private RadioMenuItem greenItem;
+    private RadioMenuItem blueItem;
+    private RadioMenuItem yellowItem;
+    private RadioMenuItem cyanItem;
+    private RadioMenuItem orangeItem;
+    private RadioMenuItem whiteItem;
+    private RadioMenuItem blackItem;
+
+
     private Alert alert;
 
     public ApplicationUI(PresentationModel presentationModel) {
@@ -37,8 +48,27 @@ public class ApplicationUI extends BorderPane {
         attributeMenu = new Menu();
 
         closeItem = new MenuItem();
-
         fileMenu.getItems().addAll(closeItem);
+
+        colorGroup = new ToggleGroup();
+        redItem = new RadioMenuItem("Red");
+        redItem.setToggleGroup(colorGroup);
+        greenItem = new RadioMenuItem("Green");
+        greenItem.setToggleGroup(colorGroup);
+        blueItem = new RadioMenuItem("Blue");
+        blueItem.setToggleGroup(colorGroup);
+        yellowItem = new RadioMenuItem("Yellow");
+        yellowItem.setToggleGroup(colorGroup);
+        cyanItem = new RadioMenuItem("Cyan");
+        cyanItem.setToggleGroup(colorGroup);
+        orangeItem = new RadioMenuItem("Orange");
+        orangeItem.setToggleGroup(colorGroup);
+        whiteItem = new RadioMenuItem("White");
+        whiteItem.setToggleGroup(colorGroup);
+        blackItem = new RadioMenuItem("Black");
+        blackItem.setToggleGroup(colorGroup);
+
+        attributeMenu.getItems().addAll(redItem, greenItem, blueItem, yellowItem, cyanItem, orangeItem, whiteItem, blackItem);
 
         MenuBar menuBar = new MenuBar();
         menuBar.getMenus().addAll(fileMenu, attributeMenu);
@@ -54,6 +84,8 @@ public class ApplicationUI extends BorderPane {
 
 
     private void setupEventHandlers() {
+
+
     }
 
     private void setupValueChangedListeners() {
@@ -77,5 +109,14 @@ public class ApplicationUI extends BorderPane {
         attributeMenu.textProperty().bind(pm.attributesMenuTitleProperty());
 
         closeItem.textProperty().bind(pm.closeMenuItemTitleProperty());
+
+        redItem.selectedProperty().bindBidirectional(pm.redBoolProperty());
+        greenItem.selectedProperty().bindBidirectional(pm.greenBoolProperty());
+        blueItem.selectedProperty().bindBidirectional(pm.blueBoolProperty());
+        yellowItem.selectedProperty().bindBidirectional(pm.yellowBoolProperty());
+        cyanItem.selectedProperty().bindBidirectional(pm.cyanBoolProperty());
+        orangeItem.selectedProperty().bindBidirectional(pm.orangeBoolProperty());
+        whiteItem.selectedProperty().bindBidirectional(pm.whiteBoolProperty());
+        blackItem.selectedProperty().bindBidirectional(pm.blackBoolProperty());
     }
 }
