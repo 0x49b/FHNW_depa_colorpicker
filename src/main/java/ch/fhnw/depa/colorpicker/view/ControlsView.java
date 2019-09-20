d3ae5f171eb69a47f0bfc8ab0a2c47ef89df0b68
Florian Thiévent <florianjeremias@gmail.com>
2019-09-21 00:25:44 +0200

    [fth] added UI Items and Methods

--- src/main/java/ch/fhnw/depa/colorpicker/view/ControlsView.java
+++ src/main/java/ch/fhnw/depa/colorpicker/view/ControlsView.java
@@ -8,9 +8,6 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.StrokeType;
-import javafx.util.converter.IntegerStringConverter;
-
-import java.util.function.UnaryOperator;
 
 public class ControlsView extends GridPane {
 
@@ -38,6 +35,8 @@ public class ControlsView extends GridPane {
     private RadioButton orangeRadio;
     private RadioButton whiteRadio;
     private RadioButton blackRadio;
+
+    private Label hexLabel;
 
     public ControlsView(PresentationModel presentationModel) {
 
@@ -122,7 +121,9 @@ public class ControlsView extends GridPane {
         blackRadio = new RadioButton("black");
         blackRadio.setUserData("BLACK");
         blackRadio.setToggleGroup(radioGroup);
-        blackRadio.fire();
+        blackRadio.setSelected(true);
+
+        hexLabel = new Label("yep");
     }
 
     private void layoutControls() {
@@ -140,6 +141,7 @@ public class ControlsView extends GridPane {
 
         // Here must be the rectangle
         add(cRect, 0, 3, 1, 5);
+        add(hexLabel, 0, 10);
 
         // Here is the space for the RadioButton
         add(redRadio, 1, 3);
@@ -158,7 +160,7 @@ public class ControlsView extends GridPane {
     private void setupEventHandlers() {
 
         darkerButton.setOnAction(event -> {
-            if (pm.getrCI() >= 1) {
+            if (pm.getrCI() > 1) {
                 pm.setrCI(pm.getrCI() - 2);
             } else {
                 pm.setrCI(0);
@@ -197,29 +199,75 @@ public class ControlsView extends GridPane {
 
     private void setupValueChangedListeners() {
 
-        rValue.textProperty().addListener((observable) -> {
-            rHex.setText(Integer.toHexString(pm.rCIProperty().get()).toUpperCase());
-            updateRectColor(null);
+        pm.hexColorProperty().addListener(event -> {
+            updateRectColor();
+            setRadioButton();
+
         });
-
-        gValue.textProperty().addListener((observable -> {
-            gHex.setText(Integer.toHexString(pm.gCIProperty().get()).toUpperCase());
-            updateRectColor(null);
-        }));
-
-        bValue.textProperty().addListener((observable -> {
-            bHex.setText(Integer.toHexString(pm.bCIProperty().get()).toUpperCase());
-            updateRectColor(null);
-        }));
 
         radioGroup.selectedToggleProperty().addListener(
                 (ObservableValue<? extends Toggle> ov, Toggle old_toggle, Toggle new_toggle) -> {
                     if (radioGroup.getSelectedToggle() != null) {
-                        System.out.println(radioGroup.getSelectedToggle().getUserData());
-                        updateRectColor(Color.);
+                        setColor(radioGroup.getSelectedToggle().getUserData().toString());
+
+                        if (redRadio == new_toggle) {
+                            pm.setRedBool(true);
+                        } else if (greenRadio == new_toggle) {
+                            pm.setGreenBool(true);
+                        } else if (blueRadio == new_toggle) {
+                            pm.setBlueBool(true);
+                        } else if (yellowRadio == new_toggle) {
+                            pm.setYellowBool(true);
+                        } else if (cyanRadio == new_toggle) {
+                            pm.setCyanBool(true);
+                        } else if (orangeRadio == new_toggle) {
+                            pm.setOrangeBool(true);
+                        } else if (whiteRadio == new_toggle) {
+                            pm.setWhiteBool(true);
+                        } else if (blackRadio == new_toggle) {
+                            pm.setBlackBool(true);
+                        }
+
+                        //old_toggle.setSelected(false);
+
                     }
+
                 }
         );
+    }
+
+
+    private void setRadioButton() {
+        switch (pm.getHexColor()) {
+            case "#FF0000":
+                redRadio.fire();
+                break;
+            case "#00FF00":
+                greenRadio.fire();
+                break;
+            case "#0000FF":
+                blueRadio.fire();
+                break;
+            case "#FFFF00":
+                yellowRadio.fire();
+                break;
+            case "#00FFFF":
+                cyanRadio.fire();
+                break;
+            case "#FFA000":
+                orangeRadio.fire();
+                break;
+            case "#FFFFFF":
+                whiteRadio.fire();
+                break;
+            case "#000000":
+                blackRadio.fire();
+                break;
+            default:
+                radioGroup.selectToggle(null);
+
+        }
+
     }
 
     private void setupBindings() {
@@ -229,29 +277,60 @@ public class ControlsView extends GridPane {
 
         rSlider.valueProperty().bindBidirectional(pm.rCIProperty());
         rValue.textProperty().bindBidirectional(pm.rCSProperty());
+        rHex.textProperty().bind(pm.rCHProperty());
 
         gSlider.valueProperty().bindBidirectional(pm.gCIProperty());
         gValue.textProperty().bindBidirectional(pm.gCSProperty());
+        gHex.textProperty().bind(pm.gCHProperty());
 
         bSlider.valueProperty().bindBidirectional(pm.bCIProperty());
         bValue.textProperty().bindBidirectional(pm.bCSProperty());
+        bHex.textProperty().bind(pm.bCHProperty());
+
+        hexLabel.textProperty().bind(pm.hexColorProperty());
+
+        redRadio.selectedProperty().bindBidirectional(pm.redBoolProperty());
+        greenRadio.selectedProperty().bindBidirectional(pm.greenBoolProperty());
+        blueRadio.selectedProperty().bindBidirectional(pm.blueBoolProperty());
+        yellowRadio.selectedProperty().bindBidirectional(pm.yellowBoolProperty());
+        cyanRadio.selectedProperty().bindBidirectional(pm.cyanBoolProperty());
+        orangeRadio.selectedProperty().bindBidirectional(pm.orangeBoolProperty());
+        whiteRadio.selectedProperty().bindBidirectional(pm.whiteBoolProperty());
+        blackRadio.selectedProperty().bindBidirectional(pm.blackBoolProperty());
     }
 
-    private void updateRectColor(Color c) {
-        if(c == null) {
-            c = Color.rgb(pm.rCIProperty().getValue(), pm.gCIProperty().getValue(), pm.bCIProperty().getValue());
-        }
+    private void updateRectColor() {
+        Color c = Color.rgb(pm.rCIProperty().getValue(), pm.gCIProperty().getValue(), pm.bCIProperty().getValue());
         cRect.setFill(c);
     }
 
-    private void checkBrighterButton() {
+    public void setColor(String color) {
+        switch (color) {
+            case "RED":
+                pm.setRGB(255, 0, 0);
+                break;
+            case "GREEN":
+                pm.setRGB(0, 255, 0);
+                break;
+            case "BLUE":
+                pm.setRGB(0, 0, 255);
+                break;
+            case "YELLOW":
+                pm.setRGB(255, 255, 0);
+                break;
+            case "CYAN":
+                pm.setRGB(0, 255, 255);
+                break;
+            case "ORANGE":
+                pm.setRGB(255, 160, 0);
+                break;
+            case "WHITE":
+                pm.setRGB(255, 255, 255);
+                break;
+            default:
+                pm.setRGB(0, 0, 0);
 
-        if (pm.rCIProperty().get() == 0 && pm.gCIProperty().get() != 0 && pm.bCIProperty().get() != 0) {
-            darkerButton.setDisable(false);
-        } else {
-            darkerButton.setDisable(true);
         }
-
     }
 
 }
