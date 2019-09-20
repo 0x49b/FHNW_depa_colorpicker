9c77cc82cff31df18cd5d6ac2c76801b76de8c60
Florian Thiévent <florianjeremias@gmail.com>
2019-09-21 00:25:16 +0200

    [fth] added Properties

--- src/main/java/ch/fhnw/depa/colorpicker/model/PresentationModel.java
+++ src/main/java/ch/fhnw/depa/colorpicker/model/PresentationModel.java
@@ -1,6 +1,8 @@
 package ch.fhnw.depa.colorpicker.model;
 
+import ch.fhnw.depa.colorpicker.helper.NumberHexStringConverter;
 import javafx.beans.binding.Bindings;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
@@ -21,24 +23,212 @@ public class PresentationModel {
     private final StringProperty darkerButtonTitle = new SimpleStringProperty("Darker");
     private final StringProperty brighterButtonTitle = new SimpleStringProperty("Brighter");
 
-
     private final SimpleIntegerProperty rCI = new SimpleIntegerProperty(0);
     private final SimpleStringProperty rCS = new SimpleStringProperty("0");
+    private final SimpleStringProperty rCH = new SimpleStringProperty("0");
 
     private final SimpleIntegerProperty gCI = new SimpleIntegerProperty(0);
     private final SimpleStringProperty gCS = new SimpleStringProperty("0");
+    private final SimpleStringProperty gCH = new SimpleStringProperty("0");
 
     private final SimpleIntegerProperty bCI = new SimpleIntegerProperty(0);
     private final SimpleStringProperty bCS = new SimpleStringProperty("0");
+    private final SimpleStringProperty bCH = new SimpleStringProperty("0");
 
+    private final SimpleStringProperty hexColor = new SimpleStringProperty("");
+
+    private final SimpleBooleanProperty redBool = new SimpleBooleanProperty(false);
+    private final SimpleBooleanProperty greenBool = new SimpleBooleanProperty(false);
+    private final SimpleBooleanProperty blueBool = new SimpleBooleanProperty(false);
+    private final SimpleBooleanProperty yellowBool = new SimpleBooleanProperty(false);
+    private final SimpleBooleanProperty cyanBool = new SimpleBooleanProperty(false);
+    private final SimpleBooleanProperty orangeBool = new SimpleBooleanProperty(false);
+    private final SimpleBooleanProperty whiteBool = new SimpleBooleanProperty(false);
+    private final SimpleBooleanProperty blackBool = new SimpleBooleanProperty(false);
 
     public PresentationModel(Stage pStage) {
         primaryStage = pStage;
+        setupListener();
 
         Bindings.bindBidirectional(rCS, rCI, new NumberStringConverter());
         Bindings.bindBidirectional(gCS, gCI, new NumberStringConverter());
         Bindings.bindBidirectional(bCS, bCI, new NumberStringConverter());
 
+        Bindings.bindBidirectional(rCH, rCI, new NumberHexStringConverter());
+        Bindings.bindBidirectional(gCH, gCI, new NumberHexStringConverter());
+        Bindings.bindBidirectional(bCH, bCI, new NumberHexStringConverter());
+    }
+
+    private void setupListener() {
+
+        rCI.addListener(event -> {
+            setHexColor();
+        });
+
+        bCI.addListener(event -> {
+            setHexColor();
+        });
+
+        gCI.addListener(event -> {
+            setHexColor();
+        });
+
+    }
+
+    public void setRGB(int r, int g, int b) {
+        setrCI(r);
+        setgCI(g);
+        setbCI(b);
+    }
+
+    public void setHexColor(String hexColor) {
+        this.hexColor.set(hexColor);
+    }
+
+    public boolean isRedBool() {
+        return redBool.get();
+    }
+
+    public SimpleBooleanProperty redBoolProperty() {
+        return redBool;
+    }
+
+    public void setRedBool(boolean redBool) {
+        this.redBool.set(redBool);
+    }
+
+    public boolean isGreenBool() {
+        return greenBool.get();
+    }
+
+    public SimpleBooleanProperty greenBoolProperty() {
+        return greenBool;
+    }
+
+    public void setGreenBool(boolean greenBool) {
+        this.greenBool.set(greenBool);
+    }
+
+    public boolean isBlueBool() {
+        return blueBool.get();
+    }
+
+    public SimpleBooleanProperty blueBoolProperty() {
+        return blueBool;
+    }
+
+    public void setBlueBool(boolean blueBool) {
+        this.blueBool.set(blueBool);
+    }
+
+    public boolean isYellowBool() {
+        return yellowBool.get();
+    }
+
+    public SimpleBooleanProperty yellowBoolProperty() {
+        return yellowBool;
+    }
+
+    public void setYellowBool(boolean yellowBool) {
+        this.yellowBool.set(yellowBool);
+    }
+
+    public boolean isCyanBool() {
+        return cyanBool.get();
+    }
+
+    public SimpleBooleanProperty cyanBoolProperty() {
+        return cyanBool;
+    }
+
+    public void setCyanBool(boolean cyanBool) {
+        this.cyanBool.set(cyanBool);
+    }
+
+    public boolean isOrangeBool() {
+        return orangeBool.get();
+    }
+
+    public SimpleBooleanProperty orangeBoolProperty() {
+        return orangeBool;
+    }
+
+    public void setOrangeBool(boolean orangeBool) {
+        this.orangeBool.set(orangeBool);
+    }
+
+    public boolean isWhiteBool() {
+        return whiteBool.get();
+    }
+
+    public SimpleBooleanProperty whiteBoolProperty() {
+        return whiteBool;
+    }
+
+    public void setWhiteBool(boolean whiteBool) {
+        this.whiteBool.set(whiteBool);
+    }
+
+    public boolean isBlackBool() {
+        return blackBool.get();
+    }
+
+    public SimpleBooleanProperty blackBoolProperty() {
+        return blackBool;
+    }
+
+    public void setBlackBool(boolean blackBool) {
+        this.blackBool.set(blackBool);
+    }
+
+    public String getHexColor() {
+        return hexColor.get();
+    }
+
+    public SimpleStringProperty hexColorProperty() {
+        return hexColor;
+    }
+
+    public void setHexColor() {
+        String hex = String.format("#%02x%02x%02x", getrCI(), getgCI(), getbCI()).toUpperCase();
+        this.hexColor.set(hex);
+    }
+
+
+    public String getrCH() {
+        return rCH.get();
+    }
+
+    public SimpleStringProperty rCHProperty() {
+        return rCH;
+    }
+
+    public void setrCH(String rCH) {
+        this.rCH.set(rCH);
+    }
+
+    public String getgCH() {
+        return gCH.get();
+    }
+
+    public SimpleStringProperty gCHProperty() {
+        return gCH;
+    }
+
+    public void setgCH(String gCH) {
+        this.gCH.set(gCH);
+    }
+
+    public String getbCH() {
+        return bCH.get();
+    }
+
+    public SimpleStringProperty bCHProperty() {
+        return bCH;
+    }
+
+    public void setbCH(String bCH) {
+        this.bCH.set(bCH);
     }
 
     public Stage getPrimaryStage() {
